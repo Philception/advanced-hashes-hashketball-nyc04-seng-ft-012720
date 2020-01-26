b5249a6b109c618c67333a9d987f495051a47dcf
Phillip Henry III <philtrader71@gmail.com>
2020-01-26 02:30:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -155,14 +155,14 @@ def player_numbers(team_name)
   nums
 end
 
-def player_stats(sought_player_name)
+def player_stats(player_search)
   new_hash = {}
   game_hash.collect do |place, team|
     team.each do |attribute, _data|
       next unless attribute == :players
 
       game_hash[place][attribute].each do |player|
-        next unless player[:player_name] == sought_player_name
+        next unless player[:player_name] == player_search
 
         new_hash = player.delete_if do |k, _v|
           k == :player_name
